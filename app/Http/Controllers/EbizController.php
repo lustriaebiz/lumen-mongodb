634afdfcdd02655b61f1629661d273b55a4a2553
Ebiz <lustria.ebis@spesolution.com>
2020-03-20 23:03:45 +0700

    implement fractal

---
+++ app/Http/Controllers/EbizController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Ebiz;
+
+
+use App\Http\Transformers\EbizTransformer;
+use App\Http\Transformers\IlluminatePaginatorAdapter;
+
+
+class EbizController extends Controller
+{
+    
+    public function index()
+    {
+        $paginator = Ebiz::paginate(10);
+        $ebiz = $paginator->getCollection();
+ 
+        $response = fractal()
+                    ->collection($ebiz, new EbizTransformer())
+                    ->paginateWith(new IlluminatePaginatorAdapter($paginator))
+                    ->toArray();
+ 
+        return response()->json($response);
+    }
+
+    
+    public function create()
+    {
+        $ebiz = new Ebiz;
+        $ebiz->data = 'haha';
+        $ebiz->save();
+
+        return response()->json(['success' => true]);
+    }
+
+}
